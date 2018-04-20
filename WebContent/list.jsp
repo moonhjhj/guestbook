ccffb0d3b8c51b8aa6f61fa7d37fb53df1b7fcad
Bit <Bit@DESKTOP-ATHU2BC>
2018-04-20 17:29:53 +0900

    guestbook

---
+++ WebContent/list.jsp
@@ -0,0 +1,64 @@
+﻿<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.List"%>
+<%@page import="com.javaex.vo.GuestbookVo"%>
+<%
+	List<GuestbookVo> list = (List<GuestbookVo>) request.getAttribute("list");
+	System.out.println(list);
+%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록</title>
+</head>
+<body>
+	<form action="/guestbook/gb" method="get">
+		<input type="hidden" name="a" value="add">
+		<table border=1 width=500>
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name"></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan=4><textarea name="content" cols=60 rows=5></textarea></td>
+			</tr>
+			<tr>
+				<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
+			</tr>
+		</table>
+	</form>
+	<br />
+	<%
+		for (GuestbookVo vo : list) {
+	%>
+	<table width=510 border=1>
+		<tr>
+			<td><%=vo.getNo()%></td>
+			<td><%=vo.getName()%></td>
+			<td><%=vo.getReg_date()%></td>
+			<td><a href="/guestbook/gb?a=delete&no=<%=vo.getNo()%>">삭제</a></td>
+		</tr>
+		<tr>
+			<td colspan=4><%=vo.getContent()%><br /></td>
+		</tr>
+	</table>
+	<br />
+	<%
+		}
+	%>
+<!-- 	<table width=510 border=1> -->
+<!-- 		<tr> -->
+<!-- 			<td>[1]</td> -->
+<!-- 			<td>황일영</td> -->
+<!-- 			<td>2017-01-15</td> -->
+<!-- 			<td><a href="">삭제</a></td> -->
+<!-- 		</tr> -->
+<!-- 		<tr> -->
+<!-- 			<td colspan=4>안녕하세요<br />두번째글입니다. -->
+<!-- 			</td> -->
+<!-- 		</tr> -->
+<!-- 	</table> -->
+<!-- 	<br /> -->
+</body>
+</html>
---
+++ src/com/javaex/controller/GuestbookController.java
@@ -0,0 +1,90 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.GuestbookDao;
+import com.javaex.vo.GuestbookVo;
+
+/**
+ * Servlet implementation class GuestbookController
+ */
+@WebServlet("/gb")
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("controller");
+		String cmd = request.getParameter("a");
+
+		if ("add".equals(cmd)) {
+
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String content = request.getParameter("content");
+			GuestbookVo vo = new GuestbookVo(name, password, content);
+			GuestbookDao dao = new GuestbookDao();
+			dao.insert(vo);
+			List<GuestbookVo> list = dao.getList();
+			request.setAttribute("list", list);
+			System.out.println("삽입 완료");
+			RequestDispatcher rd = request.getRequestDispatcher("list.jsp");
+			rd.forward(request, response);
+			
+//
+//			DateFormat reg_date = new SimpleDateFormat("yy/MM/dd");
+//			Date d;
+//			try {
+//				d = (Date) reg_date.parse("yy/MM/dd");
+//
+//				GuestbookVo vo = new GuestbookVo(name, password, content, d);
+//				// GuestbookVo vo = new GuestbookVo(name, password, content, reg_date);
+//				GuestbookDao dao = new GuestbookDao();
+//				dao.insert(vo);
+//				List<GuestbookVo> list = dao.getList();
+//
+//				request.setAttribute("list", list);
+//				// RequestDispatcher rd = request.getRequestDispatcher("list.jsp");
+//				// rd.forward(request, response);
+//
+//				System.out.println("삽입 완료");
+//				response.sendRedirect("/guestbook/gb");
+//
+//			} catch (ParseException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+
+		} else if ("delete".equals(cmd)) {
+
+		} else {
+			 GuestbookDao dao = new GuestbookDao();
+			 List<GuestbookVo> list = dao.getList();
+			
+			 request.setAttribute("list", list);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("list.jsp");
+			rd.forward(request, response);
+
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
